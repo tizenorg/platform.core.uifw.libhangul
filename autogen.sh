81b74f4bcc4c3374f56e856531defcbe62515be7
Jihoon Kim <jihoon48.kim@samsung.com>
2013-01-09 15:04:52 +0900

    upgrade libhangul-0.1.0
    Change-Id: Iffa902322555053473f933ad10c5e00b50d7a4c4

--- autogen.sh
+++ autogen.sh
@@ -3,13 +3,13 @@
 
 test -f ChangeLog || touch ChangeLog
 
-libtoolize=`which glibtoolize`
-if test -z "$libtoolize"; then
-    libtoolize=libtoolize
-fi
+#libtoolize=`which glibtoolize`
+#if test -z "$libtoolize"; then
+#    libtoolize=libtoolize
+#fi
 
 autopoint --force
-$libtoolize --automake --copy --force
+libtoolize --automake --copy --force
 aclocal $ACLOCAL_AMFLAGS
 autoheader
 automake --add-missing --copy
