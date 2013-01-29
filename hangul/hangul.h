ed204a842bfbed438d36a97d93df1cdb0050e0a5
Jihoon Kim <jihoon48.kim@samsung.com>
2013-01-29 18:53:26 +0900

    rollback to original code
    Change-Id: Icfbe9ef9159e0c0e0c699907a895cbe0bb5905ca

--- hangul/hangul.h
+++ hangul/hangul.h
@@ -75,15 +75,6 @@ typedef struct _HangulCombination     HangulCombination;
 typedef struct _HangulBuffer          HangulBuffer;
 typedef struct _HangulInputContext    HangulInputContext;
 
-typedef void   (*HangulOnTranslate)  (HangulInputContext*,
-				      int,
-				      ucschar*,
-				      void*);
-typedef bool   (*HangulOnTransition) (HangulInputContext*,
-				      ucschar,
-				      const ucschar*,
-				      void*);
-
 enum {
     HANGUL_OUTPUT_SYLLABLE,
     HANGUL_OUTPUT_JAMO
@@ -130,12 +121,6 @@ void hangul_ic_set_combination(HangulInputContext *hic,
 			       const HangulCombination *combination);
 void hangul_ic_connect_callback(HangulInputContext* hic, const char* event,
 				void* callback, void* user_data);
-void hangul_ic_connect_translate (HangulInputContext* hic,
-                             HangulOnTranslate callback,
-                             void* user_data);
-void hangul_ic_connect_transition(HangulInputContext* hic,
-                             HangulOnTransition callback,
-                             void* user_data);
 
 unsigned    hangul_ic_get_n_keyboards();
 const char* hangul_ic_get_keyboard_id(unsigned index_);
